df36486f420fbefd679a80a1c90b953d5ef3fc8f
Jay Prakash OSFIGO <jayprakashutw@outlook.com>
2025-07-09 11:05:38 +0530

    Commitallcode

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Team !!!");
+            Console.ReadKey();
+        }
+    }
+}
