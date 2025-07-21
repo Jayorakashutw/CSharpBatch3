e6a7b899f90218ec803af0ae6a04306b9ce84373
Jay Prakash OSFIGO <jayprakashutw@outlook.com>
2025-07-21 11:27:17 +0530

    From loop till array done

---
+++ ArrayExamples/ArrayPractice.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.ArrayExamples
+{
+    public class ArrayPractice
+    {
+        int[] array= { 45,85,35,4,65,25,42,65,415,8};
+        int i;
+        int sh;
+        //wap to accept 10 numbers in Array and Display
+        public void Pro1Array()
+        {
+            Console.WriteLine("Enter 10 numbers in Array");
+            for(i=0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("Stored Value in Array");
+
+            foreach(int num in array)
+            {
+                Console.Write(num+"\t");
+            }
+        }
+
+        //wap to accept 10 numbers in Array and Display only even
+        public void Pro2Array()
+        {
+            Console.WriteLine("Enter 10 numbers in Array");
+            for (i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("Stored Value in Array");
+
+            foreach (int num in array)
+            {
+                if(num%2==0)
+                    Console.Write(num + "\t");
+            }
+        }
+        //wap to display greatest in array
+        public void MaxinArray()
+        {
+            int max = array[0];
+            foreach(int num in array)
+            {
+                if(num>max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine("Greatest in Array is " + max);
+        }
+
+        //wap to sort an array using bubble /selection 
+
+        public void BubbleSortArray()
+        {
+            int len = array.Length;
+            int temp;
+            Console.WriteLine("Array before sorting");
+            foreach (int num in array)
+            {
+                Console.Write(num+"\t");
+            }
+           
+            //bubble sort logic
+
+            for(int i=0;i<len-1;i++)
+            {
+                for(int j=0;j<len-1-i;j++)
+                {
+                    if(array[j]>array[j+1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\nArray After sorting");
+            foreach (int num in array)
+            {
+                Console.Write(num + "\t");
+            }
+        }
+
+        public void SelectionSortArray()
+        {
+            int len = array.Length;
+            int temp;
+            Console.WriteLine("Array before sorting");
+            foreach (int num in array)
+            {
+                Console.Write(num + "\t");
+            }
+
+            //selction sort logic
+            
+            for (int i = 0; i < len - 1; i++)
+            {
+                for (int j = i+1; j < len ; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\nArray After sorting");
+            foreach (int num in array)
+            {
+                Console.Write(num + "\t");
+            }
+        }
+
+        public void LinearSearch()
+        {
+            Console.WriteLine("enter a number which you want to search");
+            sh = Convert.ToInt32(Console.ReadLine());
+
+            bool found=false;
+
+            for(int i=0;i<array.Length;i++)
+            {
+                if (array[i]==sh)
+                {
+                    Console.WriteLine("Elent fount at Index " + i);
+                    found=true;
+                    break;
+                }
+            }
+
+            if(!found)
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+
+        public void BinarySearch()
+        {
+            int[] arr = { 10, 15, 25, 35, 45 };
+            int min = 0;
+            int max = arr.Length - 1;
+            bool found = false;
+
+            Console.WriteLine("Enter a number you want to search");
+            sh = Convert.ToInt32(Console.ReadLine());
+            while(min<=max)
+            {
+                int mid = (min + max) / 2;
+                if (arr[mid]==sh)
+                {
+                    Console.WriteLine("Elent fount at Index " + mid);
+                    found = true;
+                    break;
+                }
+                else if (arr[mid]<sh)
+                {
+                    min = mid + 1;
+                }
+                else
+                {
+                    max = mid - 1;
+                }
+            }
+            if(!found)
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+        public void InbuildArcrayFunction()
+        {
+            Console.WriteLine("Orignal Array");
+            foreach (int num in array)
+            {
+                Console.Write(num + "\t");
+            }
+            Array.Reverse(array);
+
+            Console.WriteLine("\n Array After Reverse");
+            foreach (int num in array)
+            {
+                Console.Write(num + "\t");
+            }
+
+            Array.Sort(array);
+            Console.WriteLine("\n Array After Sort");
+            foreach (int num in array)
+            {
+                Console.Write(num + "\t");
+            }
+
+           int res= Array.BinarySearch(array,85);
+            Console.WriteLine("\n Value found at index  " +res);
+
+          
+        }
+    }
+}
---
+++ Looping/LoopingExamples.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Looping
+{
+    public class LoopingExamples
+    {
+        int num;
+        public void NaturalNoWhile()
+        {
+            num = 1; //initilization
+            while (num <= 10)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+        public void NaturalNoFor()
+        {
+            for(num=1;num<=10; num++)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public void NaturalNoDowhile()
+        {
+            num = 51;
+            do
+            {
+                Console.WriteLine(num);
+                num++;
+            }while (num <= 10);
+        }
+
+        //wap to accept a number and display its table
+
+        public void TableOfNumber()
+        {
+            Console.WriteLine("Enter a number whose table you want");
+            int tableno = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Table of {0} is below",tableno);
+
+            for(num=1;num<=10;num++)
+            {
+                Console.WriteLine(num*tableno);
+            }
+        }
+
+        //wap to accept a number and check whether it is Prime or not
+
+        public void PrimeNo()
+        {
+            Console.WriteLine("Enter a number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isprime = true;
+            if(number<=1)
+            {
+                isprime = false;
+            }
+            else
+            {
+                for(num=2;num<number;num++)
+                {
+                    if(number%num==0)
+                    {
+                        isprime = false;
+                        break;
+                    }
+                }
+            }
+
+            if(isprime)
+            {
+                Console.WriteLine("Prime No");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime No");
+            }
+        }
+
+        //interview questions
+        //121 , 12321 , 785 -> 587  , 161 ->161
+
+        public void reverseno()
+        {
+            int res = 0,rem;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            while(num!=0)
+            {
+                rem = num % 10;
+                Console.WriteLine( rem );
+                res = res * 10 + rem;
+                Console.WriteLine(res);
+                num = num / 10;
+                Console.WriteLine(num);
+                Console.WriteLine("______________________________");
+            }
+
+            Console.WriteLine("Finally Number after reverse is " + res);
+        }
+        public void Palindrom()
+        {
+            int res = 0, rem;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            int temp = num;
+            while (num != 0)
+            {
+                rem = num % 10;
+                res = res * 10 + rem;
+                num = num / 10;
+            }
+            if(temp==res)
+            {
+                Console.WriteLine("Number is palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Number is not palindrom");
+            }
+        }
+
+        //wap to display sum of all the digit of a number
+
+        public void DigitSum()
+        {
+            int res = 0, rem;
+            Console.WriteLine("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                rem = num % 10;
+                Console.WriteLine(rem);
+                res = res + rem;
+                Console.WriteLine(res);
+                num = num / 10;
+                Console.WriteLine(num);
+                Console.WriteLine("______________________________");
+            }
+
+            Console.WriteLine("Finally Sum of Digit " + res);
+        }
+
+        //wap to creat right angle triangle
+        /*
+            *
+            **
+            ***
+            ****
+            *****
+         */
+
+        public void Pattern1()
+        {
+            int i, j;
+            for(i = 1; i <= 5; i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /*
+            *****
+            ****
+            ***
+            **
+            *
+         */
+        public void Pattern2()
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 5; j >= i; j--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+        /*
+            1
+            12
+            123
+            1234
+            12345
+        */
+
+        public void Pattern3()
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(i+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        /*
+            1 
+            2 3
+            4 5 6
+            7 8 9 10
+            11 12 13 14 15
+         
+         */
+
+        public void Pattern4()
+        {
+            int i, j,num=1;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(num++ + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
